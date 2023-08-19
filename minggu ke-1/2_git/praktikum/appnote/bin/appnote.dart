ae5e0a07a43c003aeb6f1aca7bc9f93b89ef19a3
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-08-19 20:26:54 +0700

    create function catatan diary puspita

---
+++ minggu ke-1/2_git/praktikum/appnote/bin/appnote.dart
@@ -0,0 +1,12 @@
+import 'package:appnote/appnote.dart' as appnote;
+
+void catatan(String judul, String isi) {
+  print("Diary Puspita");
+  print("Judul : $judul");
+  print(isi);
+}
+
+void main(List<String> arguments) {
+  print('Hallooo. Are you okay?');
+  catatan("17 Agustus 2023", "Hii, tau ga sih kalau ini hari kemerdekaan?");
+}
---
+++ minggu ke-1/2_git/praktikum/appnote/test/appnote_test.dart
@@ -0,0 +1,8 @@
+import 'package:appnote/appnote.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

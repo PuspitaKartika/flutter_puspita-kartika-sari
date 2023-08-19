f42e8ad6aa5c15b2ff54fcfd2d1d20492950e08a
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-08-19 21:18:15 +0700

    create function deadline tugas

--- minggu ke-1/2_git/praktikum/appnote/bin/appnote.dart
+++ minggu ke-1/2_git/praktikum/appnote/bin/appnote.dart
@@ -6,7 +6,14 @@ void catatan(String judul, String isi) {
   print(isi);
 }
 
+void tugas(String task, String deadline) {
+  print("Tugas : $task");
+  print("Deadline : $deadline");
+}
+
 void main(List<String> arguments) {
   print('Hallooo. Are you okay?');
   catatan("17 Agustus 2023", "Hii, tau ga sih kalau ini hari kemerdekaan?");
+  print("\nKumpulantugas ");
+  tugas("RPL", "20 Agustus 2023");
 }

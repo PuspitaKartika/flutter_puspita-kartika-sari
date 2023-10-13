b1f1ef872c88d3b7d417aebc98093441a26e1fe6
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-14 00:49:17 +0700

    [minggu ke 8] soal MVVM menggunakan bloc

---
+++ minggu ke-8/2_MVVM Architecture/praktikum/soal_mvvm/lib/view/galeri/galeri_page.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/theme.dart';
+
+class GaleriPage extends StatefulWidget {
+  const GaleriPage({super.key});
+
+  @override
+  State<GaleriPage> createState() => _GaleriPageState();
+}
+
+class _GaleriPageState extends State<GaleriPage> {
+  static List<String> imageList = [
+    'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
+    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
+    'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: lightPurple,
+          title: const Text("Galeri"),
+          centerTitle: true,
+        ),
+        drawer: Drawer(
+          child: SafeArea(
+            child: ListView(
+              padding: const EdgeInsets.all(30),
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    child: const Text("Contact")),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/galeri');
+                    },
+                    child: const Text("Galeri"))
+              ],
+            ),
+          ),
+        ),
+        body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemCount: imageList.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.8,
+                          padding: const EdgeInsets.all(30),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Gambar ke-${index + 1}",
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Container(
+                                height: 300,
+                                width: 300,
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(imageList[index]))),
+                              ),
+                              OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) => AlertDialog(
+                                              title: const Text(
+                                                  "Apakah anda ingin pindah halaman?"),
+                                              actions: [
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.popAndPushNamed(
+                                                          context,
+                                                          '/detailGaleri',
+                                                          arguments:
+                                                              imageList[index]);
+                                                    },
+                                                    child: const Text("Ya")),
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: const Text("Tidak")),
+                                              ],
+                                            ));
+                                  },
+                                  child: const Text("Show Dialog")),
+                            ],
+                          ),
+                        );
+                      });
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Image.network(
+                    imageList[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            }));
+  }
+}

3e1ccbe04a9dd70cfa84be5d0998de1401be713a
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-14 00:25:24 +0700

    [minggu ke 7] Soal Prioritas 1

---
+++ minggu ke-7/3_Storage/praktikum/soal_prioritas1/lib/auth/login_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _nameEcd = TextEditingController();
+  final _emailEdc = TextEditingController();
+
+  late SharedPreferences loginData;
+  late bool newUser;
+
+  @override
+  void initState() {
+    super.initState();
+    checkLogin();
+  }
+
+  void checkLogin() async {
+    loginData = await SharedPreferences.getInstance();
+    newUser = loginData.getBool('login') ?? true;
+
+    if (newUser == false) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushNamed(context, '/home');
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameEcd.dispose();
+    _emailEdc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _nameEcd,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _emailEdc,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(30),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+            onPressed: () {
+              String username = _nameEcd.text;
+              loginData.setBool('login', false);
+              loginData.setString('username', username);
+              Navigator.pushNamed(context, '/home');
+            },
+            child: const Text(
+              "Submit",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            )),
+      ),
+    );
+  }
+}

95b7c03e6060d58fffbe66e964eda5edeec0d88c
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-10-24 23:12:00 +0700

    [Minggu ke 9], resume open AI

---
+++ mini-project/my_al_quran_digital/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/puspita/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/puspita/4ac11085-b020-4978-bb3a-baf88760becc/altera/fe_puspita-kartika-sari/mini-project/my_al_quran_digital"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"

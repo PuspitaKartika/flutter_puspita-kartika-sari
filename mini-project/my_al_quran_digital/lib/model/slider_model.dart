e34c8327668393081b22f141195a5b36dcdce3ce
Puspita Kartika Sari <91021761+PuspitaKartika@users.noreply.github.com>
2023-11-02 10:13:49 +0700

    add image to slider

--- mini-project/my_al_quran_digital/lib/model/slider_model.dart
+++ mini-project/my_al_quran_digital/lib/model/slider_model.dart
@@ -1,7 +1,7 @@
 List<String> slider = [
   "assets/quotes/quotes1.png",
-  "assets/quotes/quotes1.png",
-  "assets/quotes/quotes1.png",
-  "assets/quotes/quotes1.png",
-  "assets/quotes/quotes1.png",
+  "assets/quotes/quotes2.jpg",
+  "assets/quotes/quotes3.jpg",
+  "assets/quotes/quotes4.webp",
+  "assets/quotes/quotes4.jpg",
 ];
